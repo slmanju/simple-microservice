ac39e7bcb6f245ac62df47f63d2a47c466852afb
Manjula Jayawardana <manjulajayawardana@gmail.com>
2019-06-23 14:36:57 +0530

    add sleuth distributed tracing library.

--- service-a/src/main/java/com/slmanju/servicea/controller/MessageController.java
+++ service-a/src/main/java/com/slmanju/servicea/controller/MessageController.java
@@ -2,11 +2,15 @@ package com.slmanju.servicea.controller;
 
 import com.slmanju.servicea.dto.Message;
 import com.slmanju.servicea.service.MessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageController.class);
 
     private final MessageService messageService;
 
@@ -16,7 +20,14 @@ public class MessageController {
 
     @GetMapping(value = { "", "/", "/message"})
     public Message index() {
+        LOGGER.info("get message");
         return messageService.message();
+    }
+
+    @GetMapping(value = { "/hello"})
+    public Message hello() {
+        LOGGER.info("serving hello");
+        return new Message("Hello Tracing");
     }
 
 }
--- service-b/src/main/java/com/slmanju/serviceb/controller/MessageController.java
+++ service-b/src/main/java/com/slmanju/serviceb/controller/MessageController.java
@@ -3,6 +3,8 @@ package com.slmanju.serviceb.controller;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageController.class);
+
     @Value("${server.port}")
     private String port;
 
     @GetMapping(value = { "", "/", "/message"})
     public Message index() {
+        LOGGER.info("serving message from b");
         return new Message("Hello from Service B :: " + port);
     }
 
